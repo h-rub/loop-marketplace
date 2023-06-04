50c9807503ed6956f735d2ae7e017431c6dd2677
h-rub <rubio@loopgk.com>
2023-06-04 13:38:09 -0600

    Se agregó modelo productos.

--- core/products/admin.py
+++ core/products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Photo, Tag, Category
 
-# Register your models here.
+class PhotoInline(admin.StackedInline):
+    model = Photo
+
+class TagInline(admin.TabularInline):
+    model = Tag.product_set.through
+
+class CategoryInline(admin.TabularInline):
+    model = Category.product_set.through
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline, TagInline, CategoryInline]
+
+admin.site.register(Photo)
+admin.site.register(Tag)
+admin.site.register(Category)
--- core/products/models.py
+++ core/products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Product(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    tags = models.ManyToManyField('Tag')
+    categories = models.ManyToManyField('Category')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+
+class Photo(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='photos')
+    image = models.ImageField(upload_to='product_photos')
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
