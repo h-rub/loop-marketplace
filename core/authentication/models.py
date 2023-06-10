600960f47fce2065c6bafa97ed7ca839a6d2cb02
h-rub <rubio@loopgk.com>
2023-06-10 11:47:51 -0600

    Agregamos UUID al modelo CustomUser

--- core/authentication/models.py
+++ core/authentication/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 from django.contrib.auth.models import AbstractUser
 
@@ -6,8 +7,9 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
 
     class Meta:
-        ordering = ['pk']
+        ordering = ['-id']
 
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     username = models.CharField(max_length=50, null = True, blank = True)
     email = models.EmailField(max_length=130, unique = True, null = False)
     phone_number = models.CharField(max_length=13, null = True, blank = True)
@@ -16,7 +18,7 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = ['username', 'password']
     
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.email}"
+        return f"{self.first_name} {self.last_name} - {self.email} - UUID: {self.uuid}"
 
 class ProfileType(models.Model):
     """
@@ -30,5 +32,5 @@ class ProfileType(models.Model):
         return self.profile_type
 
 class Profile(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete = models.CASCADE)
     profile_type = models.ForeignKey(ProfileType, on_delete = models.PROTECT)
